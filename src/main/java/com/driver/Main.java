0c5c7efb6c07e27c311c5dea4d115104269466a5
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-08-16 18:41:04 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -11,15 +11,15 @@ public class Main {
 
   public void Product{
     public int Product(int x, int y){
-    
+      return x+y;
     }
 
     public int Product(int x, int y, int z){
-    
+      return x+y+z;
     }
 
     public double Product(double x, double y){
-    
+      return x+y;
     }
   }
 }
