199da3a388b0a66a467f96337f2a08672d49f5d2
Aniket Birambole <88453260+Aniketb1811@users.noreply.github.com>
2023-08-16 18:38:45 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+  public static void main(String []args){
+    Product p = new Product();
+    p.Product();
+    p.Product(4, 8);
+    p.Product(5, 10, 15);
+    p.Product(2l, 4l);
+  }
 
+  public void Product{
+    public int Product(int x, int y){
+    
+    }
+
+    public int Product(int x, int y, int z){
+    
+    }
+
+    public double Product(double x, double y){
+    
+    }
+  }
 }
