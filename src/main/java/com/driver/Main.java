166bd0fe7d6ad5a13808bc9a68dbd9c3e257f943
Aniket Birambole <aniketb1811@gmail.com>
2023-08-17 10:46:44 +0530

    First commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,23 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String []args){
     Product p = new Product();
-    p.Product();
-    p.Product(4, 8);
-    p.Product(5, 10, 15);
-    p.Product(2l, 4l);
-  }
-
-  public void Product{
-    public int Product(int x, int y){
-      return x+y;
-    }
-
-    public int Product(int x, int y, int z){
-      return x+y+z;
-    }
-
-    public double Product(double x, double y){
-      return x+y;
-    }
+    int i = p.Product(4, 8);
+    int j = p.Product(5, 10, 15);
+    double k = p.Product(2L, 4L);
   }
 }
---
+++ src/main/java/com/driver/Product.java
@@ -0,0 +1,20 @@
+package com.driver;
+
+public class Product {
+    public Product() {
+
+    }
+
+    public int Product(int x, int y){
+        return x + y;
+    }
+
+
+    public int Product(int x, int y, int z){
+        return x+y+z;
+    }
+
+    public double Product(double x, double y){
+        return x+y;
+    }
+}
