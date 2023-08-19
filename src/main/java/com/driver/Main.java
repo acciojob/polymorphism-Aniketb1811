c370d5ca4c5c9bc8caf4fc8e33194ada20c31164
Aniket Birambole <aniketb1811@gmail.com>
2023-08-19 14:08:42 +0530

    Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,14 +3,9 @@ package com.driver;
 public class Main {
 
   public static class Product {
-    public Product() {
-
-    }
-
     public int product(int x, int y){
       return x + y;
     }
-
 
     public int product(int x, int y, int z){
       return x+y+z;
@@ -22,8 +17,8 @@ public class Main {
   }
   public static void main(String []args){
     Product p = new Product();
-    int i = p.product(4, 8);
-    int j = p.product(5, 10, 15);
-    double k = p.product(2L, 4L);
+    p.product(4, 8);
+    p.product(5, 10, 15);
+    p.product(2L, 4L);
   }
 }
